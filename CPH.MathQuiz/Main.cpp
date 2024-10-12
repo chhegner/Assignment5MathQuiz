fe5b8d9a0eb7483c865659fff870903f4bad7d1b
Christian Hegner <300070366@fvtc.edu>
2024-10-12 17:37:55 -0500

    Add project files.

---
+++ CPH.MathQuiz/Main.cpp
@@ -0,0 +1,51 @@
+
+// Math Quiz
+// Christian Hegner
+
+//Please ensure "questions.txt" is located in the C:\temp folder for app to work correctly.
+//Please provide all answers in the form of an integer (whole number)
+
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string questions = "C:\\temp\\questions.txt"; //the text document that stores all the possible questions that can be asked
+	string quizResults = "C:\\temp\\QuizResults.txt"; //the file the questions asked and answers input get output to for grading
+	string questionAsked; //Stores the last question the user was asked in the form of a string. Changes during program runtime.
+	int userInput; //the number the user input as their answer, prior to being converted to a string for outputting to a file
+
+	// call this once before you call rand()
+	srand(time(NULL)); // initialize the random number generator
+
+	// get a random number between 1 & 10
+	cout << "Welcome to the Math Quiz! \n";
+
+	ofstream ofs(quizResults);
+
+	for (int i = 0; i < 3; i++) { cout << "Question " << i+1 << "\n";
+		ifstream ifs(questions);
+		int lineCount = 0;
+		int randomNumber = rand() % 10 + 1;
+
+		for (lineCount; lineCount < randomNumber; lineCount++) 	
+		{
+			getline(ifs, questionAsked);	
+		}
+		cout << questionAsked << " = ";
+		cin >> userInput;
+
+		ofs << questionAsked << " = " << userInput << "\n";
+		
+	}
+
+	ofs.close();
+
+	(void)_getch();
+	return 0;
+}
